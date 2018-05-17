d111048b877e60e940ca710f65c99fffd94eff2b
Anson <leungans@gmail.com>
2018-05-17 00:37:23 -0700

    implement username feature

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,8 @@ export const addMessage = msg => ({
   type: 'ADD_MESSAGE',
   payload: msg
 })
+
+export const setUser = user => ({
+  type: 'SET_USER',
+  payload: user
+})
--- src/reducers/messages-reducer.js
+++ src/reducers/messages-reducer.js
@@ -1,5 +1,7 @@
 const messages = (state = { user: '', messages: [] }, action) => {
   switch (action.type) {
+    case 'SET_USER':
+      return {...state, user: action.payload}
     case 'ADD_MESSAGE':
       return {...state, messages: state.messages.concat(action.payload)}
     default:
